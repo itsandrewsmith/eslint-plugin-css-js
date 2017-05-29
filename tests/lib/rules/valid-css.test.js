d63bad8c332719ab2932dcfc075c046bb08371b5
Andrew Smith <andrew@crowdlinker.com>
2017-05-28 20:53:49 -0400

    added value parsing via postcss and improved error reporting

--- tests/lib/rules/valid-css.test.js
+++ tests/lib/rules/valid-css.test.js
@@ -2,61 +2,67 @@
  * @fileoverview checking css in js
  * @author andrww smith
  */
-"use strict";
+'use strict'
 
 //------------------------------------------------------------------------------
 // Requirements
 //------------------------------------------------------------------------------
 
-var rule = require("../../../lib/rules/valid-css"),
-
-    RuleTester = require("eslint").RuleTester;
-
+var rule = require('../../../lib/rules/valid-css'),
+  RuleTester = require('eslint').RuleTester
 
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 
-var ruleTester = new RuleTester();
-ruleTester.run("valid-css", rule, {
+var ruleTester = new RuleTester()
+ruleTester.run('valid-css', rule, {
+  valid: [
+    "var css = { display: 'block' }",
+    "var css = { fontSize: '10px' }",
+    "var css = { height: '100px' }",
+    "var css = { width: '100%' }",
+    "var css = { borderColor: 'red' }",
+  ],
 
-    valid: [
-        "var css = { display: 'block' }",
-        "var css = { fontSize: '10px' }",
-        "var css = { height: '100px' }",
-        "var css = { width: '100%' }"
-    ],
-
-    invalid: [
+  invalid: [
+    {
+      code: "var css = { display: 'nope' }",
+      errors: [
         {
-            code: "var css = { display: 'nope' }",
-            errors: [{
-                message: "\"nope\" is not a valid value for \"display\".",
-                type: "Property"
-            }]
-        }, 
-
-        {
-            code: "var css = { fontSize: 'nope' }",
-            errors: [{
-                message: "\"nope\" is not a valid value for \"fontSize\".",
-                type: "Property"
-            }]
-        }, 
-        {
-            code: "var css = { fontSize: 10 }",
-            errors: [{
-                message: "\"10\" is not a valid value for \"fontSize\".",
-                type: "Property"
-            }]
+          message: '"nope" is not a valid value for "display".',
+          type: 'Property',
         },
+      ],
+    },
 
+    {
+      code: "var css = { fontSize: 'nope' }",
+      errors: [
         {
-            code: "var css = { width: 10 }",
-            errors: [{
-                message: "\"10\" is not a valid value for \"width\".",
-                type: "Property"
-            }]
-        }
-    ]
-});
+          message: '"nope" is not a valid value for "fontSize".',
+          type: 'Property',
+        },
+      ],
+    },
+    {
+      code: 'var css = { fontSize: 10 }',
+      errors: [
+        {
+          message: '"10" is not a valid value for "fontSize".',
+          type: 'Property',
+        },
+      ],
+    },
+
+    {
+      code: 'var css = { width: 10 }',
+      errors: [
+        {
+          message: '"10" is not a valid value for "width".',
+          type: 'Property',
+        },
+      ],
+    },
+  ],
+})
